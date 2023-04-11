4e7db9d9baf8af1c80e8dfae91366210c2c22cda
Gabriela Vargas <g.vargasr@uniandes.edu.co>
2023-04-11 15:17:46 -0500

    Cambios en PesitenciaAlohandes, SQL Alohamiento, SQL Reserva

---
+++ src/main/java/uniandes/isis2304/alohandes/persistencia/SQLAlohamiento.java
@@ -0,0 +1,26 @@
+package uniandes.isis2304.alohandes.persistencia;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+public class SQLAlohamiento {
+
+    private final static String SQL = PersistenciaAlohandes.SQL;
+	private PersistenciaAlohandes pp;
+
+    public SQLAlohamiento (PersistenciaAlohandes pp)
+	{
+		this.pp = pp;
+	}
+
+	/**
+	 * Crea y ejecuta la sentencia SQL para eliminar RESERVA de la base de datos por su fecha
+	 */
+	public long eliminarAlohamientoPorId (PersistenceManager pm, long idAlohamiento)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaAlohamiento  () + " WHERE id = ?");
+        q.setParameters(idAlohamiento);
+        return (long) q.executeUnique();            
+	}
+
+}
--- src/main/java/uniandes/isis2304/alohandes/persistencia/SQLReserva.java
+++ src/main/java/uniandes/isis2304/alohandes/persistencia/SQLReserva.java
@@ -1,5 +1,34 @@
 package uniandes.isis2304.alohandes.persistencia;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
 public class SQLReserva {
+
+    private final static String SQL = PersistenciaAlohandes.SQL;
+	private PersistenciaAlohandes pp;
+
+    public SQLReserva (PersistenciaAlohandes pp)
+	{
+		this.pp = pp;
+	}
+	
+    public long adicionarReserva (PersistenceManager pm, long idReserva, String fecha, int precio, 
+                                long idAlohamiento, long idUsuario) 
+	{
+        Query q = pm.newQuery(SQL, "INSERT INTO " + pp.darTablaReserva  () + "(id, fecha, precio, idAlohamiento, idUsuario) values (?, ?, ?, ?, ?)");
+        q.setParameters(idReserva, fecha, precio, idAlohamiento, idUsuario);
+        return (long) q.executeUnique();            
+	}
+
+	/**
+	 * Crea y ejecuta la sentencia SQL para eliminar RESERVA de la base de datos por su fecha
+	 */
+	public long eliminarReservaPorFecha (PersistenceManager pm, String fechaReserva)
+	{
+        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaReserva  () + " WHERE fecha = ?");
+        q.setParameters(fechaReserva);
+        return (long) q.executeUnique();            
+	}
 
 }
