6688bd31ba3dd608ab265bb9d19d103080b77b6b
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-08 18:44:19 -0500

    Consulta e inicio interfaz

--- src/main/java/uniandes/isis2304/alohandes/interfazApp/AlohandesInterfaz.java
+++ src/main/java/uniandes/isis2304/alohandes/interfazApp/AlohandesInterfaz.java
@@ -1,113 +1,54 @@
 package uniandes.isis2304.alohandes.interfazApp;
 
-import java.util.Scanner;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 import uniandes.isis2304.alohandes.persistencia.PersistenciaAlohandes;
 
-public class AlohandesInterfaz {
+public class AlohandesInterfaz extends JFrame{
 	
 	private PersistenciaAlohandes pa;
 	
 	public AlohandesInterfaz() {
-		pa = new PersistenciaAlohandes();		
-		Scanner sc = new Scanner(System.in);
-		generarBienvenida();
-		generarMenu(sc);
-	}
-	
-	public void generarBienvenida() {
-		System.out.println("------------BIENVENIDO A ALOHANDES------------\n\n");
-	}
-	
-	public void generarMenu(Scanner sc) {
-		System.out.println("Que operación desea realizar?");
-		System.out.println("1. Modificar la base de datos");
-		System.out.println("2. Una consulta en la base de datos");
-		System.out.println("3. Salir");
-		System.out.println("Escriba el número de la opción que desea realizar");
-		String resp1 = sc.nextLine();
-		if (resp1.equals("1")) {
-			System.out.println("\n\n");
-			System.out.println("Que operación desea realizar?");
-			System.out.println("1. Registrar una reserva");
-			System.out.println("2. Eliminar una reserva");
-			System.out.println("3. Retirar una oferta de alohamiento");
-			System.out.println("Escriba el número de la opción que desea realizar");
-			String resp2 = sc.nextLine();
-			
-			if (resp2.equals("1")) {
-				System.out.println("Escriba la fecha de la reserva:");
-				String fecha = sc.nextLine();
-				System.out.println("Escriba el precio de la reserva:");
-				int precio = Integer.parseInt(sc.nextLine());
-				System.out.println("Escriba el id del alojamiento de la reserva:");
-				long idAlohamiento = Long.parseLong(sc.nextLine());
-				System.out.println("Escriba el id del usuario de la reserva:");
-				long idUsuario = Long.parseLong(sc.nextLine());
-				System.out.println("Se agregó la reserva con el id: "+pa.adicionarReserva(fecha, precio, idAlohamiento, idUsuario));
-				System.out.println("\n\n\n");
-				generarMenu(sc);
-				
-			} else if (resp2.equals("2")) {
-				System.out.println("Se eliminó la reserva con el id: "+pa.eliminarReservaPorFecha(""));
-				System.out.println("\n\n\n");
-				generarMenu(sc);
-				
-			} else if (resp2.equals("3")) {
-				System.out.println("Escriba el id del alojamiento a eliminar:");
-				Long id = Long.parseLong(sc.nextLine());
-				System.out.println("Se eliminó el alojamiento con el id: "+pa.eliminarAlohamientoPorId(id));
-				System.out.println("\n\n\n");
-				generarMenu(sc);
-				
-			}  else {
-				System.out.println("\n\n Esa no es una opción valida, intentelo de nuevo \n\n");
-				generarMenu(sc);
-			}
-			
-			
-		} else if (resp1.equals("2")) {
-			System.out.println("\n\n");
-			System.out.println("Que operación desea realizar?");
-			System.out.println("1. Mostrar el dinero recibido por cada proovedor de alojamiento");
-			System.out.println("2. Mostrar las 20 ofertas más populares");
-			System.out.println("3. Mostrar el indice de ocupación de cada oferta");
-			System.out.println("Escriba el número de la opción que desea realizar");
-			String resp3 = sc.nextLine();
-			
-			if (resp3.equals("1")) {
-				System.out.println("Escriba el año del que desea hacer la consulta:");
-				int anhio = Integer.parseInt(sc.nextLine());
-				System.out.println(pa.darDineroRecibido(anhio));
-				System.out.println("\n\n\n");
-				generarMenu(sc);
-				
-			} else if (resp3.equals("2")) {
-				System.out.println(pa.dar20MasPopulares());
-				System.out.println("\n\n\n");
-				generarMenu(sc);
-				
-			} else if (resp3.equals("3")) {
-				System.out.println(pa.darIndiceOcupacion());
-				System.out.println("\n\n\n");
-				generarMenu(sc);
-				
-			} else {
-				System.out.println("\n\n Esa no es una opción valida, intentelo de nuevo \n\n");
-				generarMenu(sc);
-			}
-			
-		} else if (resp1.equals("3")) {
-			
-			
-		} else {
-			System.out.println("\n\n Esa no es una opción valida, intentelo de nuevo \n\n");
-			generarMenu(sc);
-		}
-	}
+        // Configuración de la ventana principal
+        setTitle("Interfaz Básica");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(0, 2)); // Dos columnas
 
-	public static void main(String[] args) {
-		AlohandesInterfaz a = new AlohandesInterfaz();
-	}
+        // Creación de los botones
+        for (int i = 1; i <= 18; i++) {
+            JButton button = new JButton("Botón " + i);
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    abrirNuevaVentana();
+                }
+            });
+            add(button);
+        }
+
+        // Mostrar la ventana
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void abrirNuevaVentana() {
+        JFrame ventanaNueva = new JFrame("Ventana Nueva");
+        ventanaNueva.setSize(300, 200);
+        ventanaNueva.setLocationRelativeTo(this);
+        ventanaNueva.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new AlohandesInterfaz();
+            }
+        });
+    }
 
 }
--- src/main/java/uniandes/isis2304/alohandes/persistencia/SQLUtil.java
+++ src/main/java/uniandes/isis2304/alohandes/persistencia/SQLUtil.java
@@ -8,6 +8,7 @@ import javax.jdo.Query;
 import uniandes.isis2304.alohandes.negocio.ListaDinero;
 import uniandes.isis2304.alohandes.negocio.ListaIndiceOcupacion;
 import uniandes.isis2304.alohandes.negocio.OfertasPopulares;
+import uniandes.isis2304.alohandes.negocio.Usuario;
 
 public class SQLUtil {
 	
@@ -35,6 +36,30 @@ public class SQLUtil {
 		Query q = pm.newQuery(SQL, "SELECT * FROM " + pa.darTablaIndiceOcupacion());
 		q.setResultClass(ListaIndiceOcupacion.class);
 		return (List<ListaIndiceOcupacion>) q.executeList();
+	}
+	
+	public List<Integer> analizarInformacion(PersistenceManager pm) {
+		Query q = pm.newQuery(SQL, "SELECT fecha, COUNT(*) AS cantidad_reservas, SUM(dinero_costo) AS ingresos FROM reservas GROUP BY fecha ORDER BY cantidad_reservas DESC, ingresos DESC");
+		q.setResultClass(ListaIndiceOcupacion.class);
+		return (List<Integer>) q.executeList();
+	}
+	
+	public List<Usuario> darClientesFrecuentes(PersistenceManager pm, String tipo, Long id) {
+		Query q = pm.newQuery(SQL, "SELECT usuarios.* FROM usuarios INNER JOIN reservas r ON u.id = r.id_usuario INNER JOIN alojamientos a ON r.id_alojamiento = a.id WHERE a." + tipo + "= " + id + "GROUP BY u.id HAVING COUNT(*) >= 3");
+		q.setResultClass(ListaIndiceOcupacion.class);
+		return (List<Usuario>) q.executeList();
+	}
+	
+	public List<OfertasPopulares> darOfertasSinDemanda(PersistenceManager pm) {
+		Query q = pm.newQuery(SQL, "SELECT * FROM alojamientos a LEFT JOIN reservas r ON a.id = r.id_alojamiento WHERE r.fecha_reserva IS NULL OR r.fecha_reserva < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)");
+		q.setResultClass(OfertasPopulares.class);
+		return (List<OfertasPopulares>) q.executeList();
+	}
+	
+	public List<Integer> darUsoAlohandes(PersistenceManager pm, Long id) {
+		Query q = pm.newQuery(SQL, "SELECT COUNT(*) AS cantidad_reservas, SUM(dinero_costo) AS total_gastado FROM reservas WHERE id_usuario = " + id);
+		q.setResultClass(ListaIndiceOcupacion.class);
+		return (List<Integer>) q.executeList();
 	}
 
     public long nextval(PersistenceManager persistenceManager) {
